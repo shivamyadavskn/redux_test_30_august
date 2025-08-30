14e0f90b0ada86db3bb2203530b171e5026edc27
shivam yadav <shivamyadavskn@gmail.com>
2025-08-30 19:34:05 +0530

    added new conflict

--- src/components/PostComp.tsx
+++ src/components/PostComp.tsx
@@ -1,26 +1,13 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const PostComp = () => {
-  const [count, setCount] = useState(20);
-
-  useLayoutEffect(() => {
-    console.log("🚀 Component mounted");
-    setCount((prev) => prev + 100);
-    console.log("🚀 Component mounted", count);
+  useEffect(() => {
+    console.log("🔥 Shivam Branch version running...");
   }, []);
-
-  useEffect(() => {
-    console.log("🔄 Count changed:", count);
-  }, [count]);
-
-  useEffect(() => {
-    console.log("✨ I run after every render");
-  });
 
   return (
     <div>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <h2>Post from Shivam Branch</h2>
     </div>
   );
 };
